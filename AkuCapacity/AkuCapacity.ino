21b683df78ff15d0b36f72933f9dad4d03a3655f
michciobb <michciobb@gmail.com>
2022-11-19 21:36:10 +0100

    Działa pomiar i wyświetlanie
    działa z wyjściem sterującym na IO10

--- AkuCapacity/AkuCapacity.ino
+++ AkuCapacity/AkuCapacity.ino
@@ -11,7 +11,11 @@
 
 #include <Wire.h>
 #include "DFRobot_INA219.h"
-//#include "SSD1306Wire.h"
+#include "SSD1306Wire.h"
+
+// Initialize the OLED display using Arduino Wire:
+SSD1306Wire display(0x3c, SDA, SCL);   // SDA jest pod IO8, SCL jest  pod IO9, ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+
 
 /**
  * @fn DFRobot_INA219_IIC
@@ -37,30 +41,86 @@ unsigned long lastTime = 0;
 // Set timer to 5 seconds (5000)
 unsigned long timerDelay = 1000;
 
+float voltage = 0;
+float current = 0;
+
+int dischargingTime_sec_ = 0;
+
+float batCapacity = 0;
+bool measurmentFlag = 1;
 
 void setup() {
   Serial.begin(115200);
-    //Open the serial port
-    while(!Serial);
-    
-    Serial.println();
-    //Initialize the sensor
-    while(ina219.begin() != true) {
-        Serial.println("INA219 begin faild");
-        delay(2000);
-    }
-    //Linear calibration
-    ina219.linearCalibrate(/*The measured current before calibration*/ina219Reading_mA, /*The current measured by other current testers*/extMeterReading_mA);
-    Serial.println();
+  //Open the serial port
+  while(!Serial);
+
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  
+  Serial.println();
+  //Initialize the sensor
+  while(ina219.begin() != true) {
+      Serial.println("INA219 begin faild");
+      delay(2000);
+  }
+  //Linear calibration
+  ina219.linearCalibrate(/*The measured current before calibration*/ina219Reading_mA, /*The current measured by other current testers*/extMeterReading_mA);
+  Serial.println();
+
+  // Initialising the UI will init the display too.
+  display.init();
+  display.clear();
+
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);// były też _10 i _16 i _24
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
 }
 
 void loop() {
-  if ((millis() - lastTime) > timerDelay) {
   
-    Serial.print("BusVoltage:   ");
-    Serial.println(ina219.getBusVoltage_V(), 2);
 
+  if ((millis() - lastTime) > timerDelay) {
+    display.clear();
+
+    voltage = ina219.getBusVoltage_V();
+    current = ina219.getCurrent_mA();
+
+    if (voltage < 7.1) {
+      measurmentFlag = false;
+      digitalWrite(10, LOW);
+    }
+    
+
+    //Serial.print("BusVoltage:   ");
+    //Serial.println(voltage, 2);
+
+    char buff[10];
+    sprintf(buff, "%.2f V", voltage);
+    display.drawString(80, 0, buff);
+
+    sprintf(buff, "%.0f mA", current);
+    display.drawString(100, 16, buff);
+
+
+    char timeBuff[10];
+    int mins = dischargingTime_sec_/60;                                        //Number of seconds in an hour
+    int sec = (dischargingTime_sec_-mins*60);                                             //Remove the number of hours and calculate the minutes.
+    sprintf(timeBuff, "%d:%02d", mins, sec);                               //formatowanie teksyu 0:00
+    display.drawString(110,32,timeBuff);
+
+
+    
+    if (measurmentFlag) batCapacity += current / 3600;
+    sprintf(buff, "%.0f mAh", batCapacity);
+    display.drawString(120, 48, buff);
+
+    if (measurmentFlag) display.drawString(50, 32, "ON"); else  display.drawString(50, 32, "OFF");
+
+    display.display();
+
+    
     lastTime = millis();
+    if (measurmentFlag) dischargingTime_sec_++;    
   }
 
 }
